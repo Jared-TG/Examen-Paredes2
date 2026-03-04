92cca6b0ff9cbb08cfc0bbd9e19cd9bc7bb9f2c6
miniCoded <agbarron20@gmail.com>
2026-03-04 08:28:19 -0700

    feat: não não você no tenei permiso

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AutService } from '../../services/aut-service';
 import { Router } from '@angular/router';   
@@ -17,6 +17,9 @@ export class Login {
   private fb = inject (FormBuilder);
   private auth = inject(AutService);
   private router = inject(Router);
+
+  public login_valido = signal<boolean>(true);
+
   form = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]],
@@ -30,6 +33,9 @@ export class Login {
     const { email, password } = this.form.getRawValue(); 
 
     const valido = this.auth.Login(email!, password!);
+    this.login_valido.set(valido);
+
+    console.log(this.login_valido())
 
     if (valido) {
       this.router.navigate(['/home']);
